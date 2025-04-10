33d9be6a913f769ede13fccfade0e2fffbb1949e
yousifpa98 <yousif.paulus@dci-student.org>
2025-04-11 01:37:19 +0200

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
-import netlify from 'vike-netlify/plugin';
 
 export default defineConfig({
-  plugins: [vike(), react({}), tailwindcss(), netlify()],
+  plugins: [vike(), react({}), tailwindcss()],
 
   build: {
     target: "es2022",
